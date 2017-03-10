81f34f782d7f68b028dd785ca9265345a64c2665
Augusto Franzoia <ajfranzoia@gmail.com>
2017-03-10 10:15:54 -0300

    Add challenge text on navbar

--- frontend-app/scripts/Navbar.js
+++ frontend-app/scripts/Navbar.js
@@ -12,20 +12,18 @@ export default function Navbar(props) {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand" href="#">DocSync</a>
+          <a className="navbar-brand">DocSync</a>
         </div>
         <div id="navbar" className="collapse navbar-collapse">
-
           <p className="navbar-text">Welcome back <strong>{props.username}</strong>!</p>
-
           <ul className="nav navbar-nav">
             <li className="dropdown">
               <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                 {props.users.length} other users connected
                 <span className="caret"></span>
               </a>
-
-              { props.users.length ?
+              {
+                props.users.length ?
                   (
                     <ul className="dropdown-menu">
                       {(
@@ -40,11 +38,10 @@ export default function Navbar(props) {
                     </ul>
                   ) : null
               }
-
             </li>
           </ul>
-
           <div className="nav navbar-nav navbar-right">
+            <p id="challenge-text" className="navbar-text">Challenge for <a href="https://mural.ly" target="_blank">Mural</a></p>
             <button type="button" className="btn btn-warning btn-sm navbar-btn" onClick={props.doLogout}>Logout</button>
           </div>
         </div>
